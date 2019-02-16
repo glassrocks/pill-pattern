f5954fc6c73c85a1022f580dd8b0f69dde41daed
Ian Costa <sammythebeast18@gmail.com>
2019-02-16 17:00:37 -0500

    Update pill-tracker.py

--- pill-tracker.py
+++ pill-tracker.py
@@ -1,26 +1,44 @@
 import RPi.GPIO as GPIO
 import time
-pin_list = (6, 13, 19, 26, 12, 16, 20)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(6, GPIO.IN)
+import threading
 
-#initialise a previous input variable to 0 (Assume no pressure applied)
-prev_input = 0
-count = 0
-try:
-    while True:
-        #take a reading
-        input = GPIO.input(6)
-        #if the last reading was low and this one high, alert us
-        if ((not prev_input) and input):
-            print("Under Pressure")
-            count = count + 1
-            print(count)
-        #update previous input
-        prev_input = input
-        #slight pause
-        time.sleep(0.10)
-except KeyboardInterrupt:
+
+def start_reading():
+
+    pin_list = (6, 13, 19, 26, 12, 16, 20)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(6, GPIO.IN)
+
+    # initialise a previous input variable to 0 (Assume no pressure applied)
+    prev_input = 0
+    count = 0
+
+    try:
+
+        while True:
+
+            # take a reading
+            input = GPIO.input(6)
+            print(f"Type of input: {type(input)}")
+            # if the last reading was low and this one high, alert us
+            if ((not prev_input) and input):
+                print("Under Pressure")
+                count = count + 1
+                print(count)
+            # update previous input
+            prev_input = input
+            # slight pause
+            time.sleep(0.10)
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        GPIO.cleanup()
+
+
+def start_website():
     pass
-finally:
-    GPIO.cleanup()
+
+if __name__ == "__main__":
+    reader = threading.Thread(start_reading)
+    website = threading.Thread(start_website)
