246d0b8f5d1f7641db118f6058b7dab17e82c932
glassrocks <glassrocks@protonmail.com>
2019-02-17 00:30:27 -0500

    updated python file

--- pill-tracker.py
+++ pill-tracker.py
@@ -6,27 +6,43 @@ from flask import Flask, render_template
 
 def start_reading():
 
-    PINS = (6, 13, 19, 26, 12, 16, 20)
+    PINLIST= (6, 13, 19, 26, 12, 16, 20)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(6, GPIO.IN)
+    GPIO.setup(PINLIST[0], GPIO.IN)
+    GPIO.setup(PINLIST[1], GPIO.IN)
 
     # initialise a previous input variable to 0 (Assume no pressure applied)
     prev_input = 0
     count = 0
+    global pinput
+    pinput = 0
 
     try:
+        
         while True:
-
-            global pinput
             # take a reading
-            pinput = GPIO.input(6)
+            pin_outputlist = list()
+            day_sun = GPIO.input(PINLIST[0])
+            day_mon = GPIO.input(PINLIST[1])
+            print("pin 1: {0}".format(day_sun))
+            print("pin 2: {0}".format(day_mon))
+            pin_outputlist.append(GPIO.input(PINLIST[0]))
+            pin_outputlist.append(GPIO.input(PINLIST[1]))
+            print("loop:")
+            for pin_output in range(len(pin_outputlist)):
+                #pinput = pinput + pin_outputlist[pin_output]
+                print(pin_output)
+            print("Pinput is: {0}".format(pinput))
             # if the last reading was low and this one high, alert us
-            if ((not prev_input) and pinput):
-                print("Under Pressure")
+            if (prev_input is not pinput):
+                print("Change detected!")
                 count = count + 1
-                print(count)
+                print("Num of changes: {0}".format(count))
+
+            input()
             # update previous input
             prev_input = pinput
+            del pin_outputlist
             # slight pause
             sleep(0.10)
 
@@ -42,7 +58,7 @@ def start_website():
     @app.route('/<string:page_name>/')
     def render_static(page_name):
         if ".jpeg" in page_name:
-            return render_template('./%s.jpeg' % page_name)
+            return render_template('./%s.jpeg' % page_name, )
         elif page_name != "favicon.ico":
             if page_name == "calendar":
                 threading.Thread(start_reading())
@@ -54,4 +70,5 @@ def start_website():
 
 
 if __name__ == "__main__":
-    website, pins = start_website()
+    #website, pins = start_website()
+    start_reading()
